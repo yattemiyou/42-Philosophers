a5761726b0e0154b5ece55653765251c84595019
Your Name <you@example.com>
2024-02-10 16:43:19 +0900

    課題の下準備

--- philo/philosopher.c
+++ philo/philosopher.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 14:07:24 by anonymous         #+#    #+#             */
-/*   Updated: 2024/02/10 15:53:44 by anonymous        ###   ########.fr       */
+/*   Updated: 2024/02/10 16:42:18 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,13 +16,12 @@ static int	do_something(t_philo *philo, char *something, int msec)
 {
 	int64_t	start_time;
 
-	start_time = get_time() - philo->sim->start_time;
-	printf("%zd %d is %s\n", start_time, philo->id, something);
-	while (msec > 0)
+	start_time = get_time();
+	printf("%zd %d is %s\n", start_time - philo->sim->start_time, philo->id, something);
+	while (get_time() - start_time < msec)
 	{
 		if (usleep(100) == -1)
 			return (printf("%s\n", MSG04), FALSE);
-		msec -= (get_time() - start_time);
 	}
 	return (TRUE);
 }
@@ -40,5 +39,6 @@ void	*run(void *arg)
 		do_something(philo, "sleeping", philo->sim->sleep);
 		philo->lifetime -= (get_time() - philo->last_meal);
 	}
+	printf("%zd %d died\n", get_time() - philo->sim->start_time, philo->id);
 	return (NULL);
 }

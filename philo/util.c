d1275e7b052b4001a799204aea1871fba509bc37
Your Name <you@example.com>
2024-03-25 06:35:34 +0900

    エラーが発生しないので

--- philo/util.c
+++ philo/util.c
@@ -6,7 +6,7 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/24 14:59:47 by anonymous         #+#    #+#             */
-/*   Updated: 2024/03/24 21:52:44 by anonymous        ###   ########.fr       */
+/*   Updated: 2024/03/25 06:34:28 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,8 +16,7 @@ int64_t	get_time(void)
 {
 	struct timeval	tv;
 
-	if (gettimeofday(&tv, NULL) == -1)
-		return (FALSE);
+	gettimeofday(&tv, NULL);
 	return (tv.tv_sec * 1000 + tv.tv_usec / 1000);
 }
 

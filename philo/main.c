985a4dc0442930004e8d170f44ae31f6e397a68f
Your Name <you@example.com>
2024-03-25 07:49:58 +0900

    哲学者の終了監視

--- philo/main.c
+++ philo/main.c
@@ -6,11 +6,29 @@
 /*   By: anonymous <anonymous@student.42tokyo.jp    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/10 14:36:08 by anonymous         #+#    #+#             */
-/*   Updated: 2024/02/11 15:23:07 by anonymous        ###   ########.fr       */
+/*   Updated: 2024/03/25 07:45:59 by anonymous        ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philosophers.h"
+
+static int	watch(t_sim *sim, t_philo *philos)
+{
+	int	i;
+
+	while (TRUE)
+	{
+		i = 0;
+		while (i < sim->number)
+			if (is_terminated(&philos[i++]))
+				break ;
+		if (i < sim->number)
+			break ;
+		if (usleep(100) == -1)
+			return (printf("%s\n", MSG04), FALSE);
+	}
+	return (TRUE);
+}
 
 int	main(int argc, char const *argv[])
 {
@@ -21,7 +39,7 @@ int	main(int argc, char const *argv[])
 		return (1);
 	if (start(&sim, philos) == FALSE)
 		return (finalize(&sim, philos), 1);
-	if (sim_wait(&sim, philos) == FALSE)
+	if (watch(&sim, philos) == FALSE)
 		return (finalize(&sim, philos), 1);
 	return (finalize(&sim, philos), 0);
 }
